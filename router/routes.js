dcb62272f3cd81c01c7d269ef323f50806918baf
Daniel Unknown <danielunknown@Daniels-MBP.attlocal.net>
2023-12-21 18:52:59 -0500

    added edit route

--- router/routes.js
+++ router/routes.js
@@ -3,10 +3,46 @@ const router = express.Router();
 
 
 
+
 router.get('/', (req, res)=>{
-    res.render('home')
+
+    const article = [{
+        title: 'title',
+        date: new Date(),
+        description : 'blog description'
+    },
+    {
+        title: 'title2',
+        date: new Date(),
+        description : 'blog description2'
+    }
+]
+    res.render('home', {
+        articles: article
+    }) 
+      
+    
 })
 
+
+//new article route
+router.get('/new', (req, res)=>{
+    res.render('new')
+})
+
+//save new post
+
+router.post('/newarticle', async (req, res)=>{
+    console.log('new post added');
+    let title = req.body.title;
+    console.log(title)
+
+})
+
+//edit route
+router.get('/edit', (req,res)=>{
+    res.render('edit')
+})
 
 
 
--- server.js
+++ server.js
@@ -2,14 +2,18 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const blogRouter = require('./router/routes')
-const bodyparser = require('body-parser')
+const bodyParser = require('body-parser')
 const ejs = require('ejs');
+const path = require('path');
 
 
 
 // middlewares
-app.use('/routes' ,blogRouter);
+app.use(bodyParser.urlencoded({extended: false}))
 app.set('view engine', 'ejs');
+app.use(express.static('public'))
+app.use('/routes' ,blogRouter);
+
 
 
 
